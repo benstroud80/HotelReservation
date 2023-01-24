4af226857fb5cd0d85805598b66c9fb0b426cf5d
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-24 10:12:38 -0800

    only on line 16

--- HotelReservation.py
+++ HotelReservation.py
@@ -1,1 +1,16 @@
+from datetime import datetime
+import locale
 
+print("The Hotel Reservation program\n")
+
+again = "y"
+while again.lower() == "y"
+    # get arrival date
+    while True:
+        date_str = input("Enter arrival date (YYY-MM-DD): ")
+        try:
+            arrival_date = datetime.strptime(date_str, "%Y-%m-%d")
+        except ValueError:
+            print("Invalid date format. Try again.")
+            print()
+            continue # skip next if statement and re-start loop
